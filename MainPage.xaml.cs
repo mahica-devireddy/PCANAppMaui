af5920148246cb41d87cc39ddb7e0528c24da0a0
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-11 10:06:29 -0400

    Update MainPage.xaml.cs
    Working main page that switches language when language icon is clicked

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,6 +4,9 @@ using System.Timers;
 using Microsoft.Maui.Controls;
 using Timer = System.Timers.Timer;
 using System.Threading.Tasks;
+using LocalizationResourceManager.Maui;
+using System.Globalization;
+using PCANAppM.Resources.Languages;
 
 #if WINDOWS
 using PCANAppMaui.Platforms.Windows;
@@ -13,18 +16,26 @@ namespace PCANAppM
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ILocalizationResourceManager _localizationResourceManager;
         private Timer deviceCheckTimer;
         private string lastStatus = "";
 
-        public MainPage()
+        public MainPage(ILocalizationResourceManager localizationResourceManager)
         {
+            _localizationResourceManager = localizationResourceManager;
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateDeviceStatus();
             StartDeviceCheckTimer();
         }
 
         private void StartDeviceCheckTimer()
         {
-            deviceCheckTimer = new Timer(1000); // 1 second interval  
+            deviceCheckTimer = new Timer(1000);
             deviceCheckTimer.Elapsed += DeviceCheckTimer_Elapsed;
             deviceCheckTimer.AutoReset = true;
             deviceCheckTimer.Start();
@@ -37,39 +48,49 @@ namespace PCANAppM
 
         private void UpdateDeviceStatus()
         {
-            // Only run on Windows  
 #if WINDOWS
-        var devices = PCAN_USB.GetUSBDevices();
-        string status;
-        string imageSource;
-        if (devices != null && devices.Count > 0)
-        {
-            status = $"{devices[0]} Device Connected";
-            imageSource = "green_check.jpg"; // Use your connected image
-        }
-        else
-        {
-            status = "PCAN Device NOT Connected.";
-            imageSource = "red_ex.jpg"; // Use your disconnected image
-        }
-
-        if (status != lastStatus)
-        {
-            lastStatus = status;
-            MainThread.BeginInvokeOnMainThread(() =>
+            var devices = PCAN_USB.GetUSBDevices();
+            string status;
+            string imageSource;
+            if (devices != null && devices.Count > 0)
             {
-                StatusLabel.Text = status;
-                if (status.Contains("Device Connected")) 
+                status = devices[0] + " " + _localizationResourceManager["Status2"];
+                imageSource = "green_check.png";
+            }
+            else
+            {
+                status = _localizationResourceManager["Status1"];
+                imageSource = "red_ex.png";
+            }
+
+            if (status != lastStatus)
+            {
+                lastStatus = status;
+                MainThread.BeginInvokeOnMainThread(() =>
                 {
+                    StatusLabel.Text = status;
                     StatusImage1.Source = imageSource;
-                } else if (status.Contains("NOT Connected")) 
-                {
-                    StatusImage1.Source = imageSource;
-                }
-    });
-}
+                });
+            }
 #endif
         }
+
+        private void OnLanguageButtonClicked(object sender, EventArgs e)
+        {
+            LanguageState.CurrentLanguage = LanguageState.CurrentLanguage == "en" ? "es" : "en";
+            _localizationResourceManager.CurrentCulture = new CultureInfo(LanguageState.CurrentLanguage);
+            UpdateDeviceStatus();
+        }
+
+        private async void OnNextButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Menu(_localizationResourceManager));
+        }
+    }
+
+    public static class LanguageState
+    {
+        public static string CurrentLanguage { get; set; } = "en";
+        public static bool IsSpanish => CurrentLanguage == "es";
     }
 }
-
