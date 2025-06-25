923533d381b273c270a99fa594cf259d2a6dfa9a
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-25 07:54:52 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using LocalizationResourceManager.Maui;
 using System.Globalization;
 using PCANAppM.Resources.Languages;
+using PCANAppM;
+
 
 #if WINDOWS
 using PCANAppM.Platforms.Windows;
@@ -20,6 +22,9 @@ namespace PCANAppM
         private Timer deviceCheckTimer;
         private string lastStatus = "";
         private bool isDeviceConnected;
+        private bool _sideMenuFirstOpen = true;
+        private bool _isLanguageGlowing = false;
+        private bool _isPointerOverLanguageButton = false;
 
         public MainPage(ILocalizationResourceManager localizationResourceManager)
         {
@@ -87,19 +92,20 @@ namespace PCANAppM
             else
             {
                 ConnectionDialog.IsVisible = true;
+                MainContent.IsVisible = false; // Hide all except header and dialog
             }
         }
 
         private void OnConnectionDialogOkClicked(object sender, EventArgs e)
         {
+            MainContent.IsVisible = true; // Show main content again
             ConnectionDialog.IsVisible = false;
         }
 
-        private async void OnLanguageButtonClicked(object sender, EventArgs e)
+        private void OnLanguageButtonClicked(object sender, EventArgs e)
         {
             LanguageState.CurrentLanguage = LanguageState.CurrentLanguage == "en" ? "es" : "en";
             _localizationResourceManager.CurrentCulture = new CultureInfo(LanguageState.CurrentLanguage);
-            //UpdateDeviceStatus();
         }
 
         private async void OnNextButtonClicked(object sender, EventArgs e)
@@ -110,6 +116,84 @@ namespace PCANAppM
         private void StatusImage1_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void OnOshkoshLogoClicked(object sender, EventArgs e)
+        {
+            SideMenu.IsVisible = true;
+            SideMenuDim.IsVisible = true;
+
+            if (SideMenu.Width == 0)
+            {
+                // Wait for the menu to be measured, then animate
+                SideMenu.SizeChanged += SideMenu_SizeChangedAnimateIn;
+            }
+            else
+            {
+                AnimateSideMenuIn();
+            }
+        }
+
+        private async void SideMenu_SizeChangedAnimateIn(object? sender, EventArgs e)
+        {
+            if (SideMenu.Width > 0)
+            {
+                SideMenu.SizeChanged -= SideMenu_SizeChangedAnimateIn;
+                await AnimateSideMenuIn();
+            }
+        }
+
+        private async Task AnimateSideMenuIn()
+        {
+            SideMenu.TranslationX = -SideMenu.Width;
+            await SideMenu.TranslateTo(0, 0, 250, Easing.SinOut);
+        }
+
+        private async void SideMenuOnFirstSizeChanged(object? sender, EventArgs e)
+        {
+            SideMenu.SizeChanged -= SideMenuOnFirstSizeChanged;
+            _sideMenuFirstOpen = false;
+            SideMenu.TranslationX = -SideMenu.Width;
+            await SideMenu.TranslateTo(0, 0, 250, Easing.SinOut);
+        }
+
+        private async void OnCloseSideMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 250, Easing.SinIn); // Slide out
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+        }
+
+        private async void OnMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new Menu(_localizationResourceManager));
+        }
+
+        private async void OnAngleSensorMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new BAS(_localizationResourceManager));
+        }
+
+        private async void OnKzValveMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new KZV(_localizationResourceManager));
+        }
+
+        private async void OnFluidTankLevelMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new FTLS(_localizationResourceManager));
         }
     }
 
