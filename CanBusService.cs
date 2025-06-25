bd1d8ca7bd3f85a2ce7bcc57a02271388d3c72ce
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-25 13:21:10 -0400

    Update CanBusService.cs

--- CanBusService.cs
+++ CanBusService.cs
@@ -1,104 +1,95 @@
-public class CanBusService : ICanBusService, INotifyPropertyChanged, IDisposable
+namespace PCANAppM.Services
 {
-    const int PollMs = 500;
-    const int DebounceForUnplug = 3;
-
-    readonly Timer _poll;
-    readonly IDispatcher _dispatcher;
-
-    PCAN_USB? _dev;
-    ushort    _h;
-    int       _absentCount;
-
-    bool _isConn;
-    public bool IsConnected { /* same as before */ }
-    public string? DeviceName { get; private set; }
-    public event Action<PCAN_USB.Packet>? FrameReceived;
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public CanBusService(IDispatcher dispatcher)
+#if WINDOWS
+    public class CanBusService : ICanBusService, INotifyPropertyChanged, IDisposable
     {
-        _dispatcher = dispatcher;
-        _poll = new Timer(PollMs) { AutoReset = true };
-        _poll.Elapsed += (_,__) => Check();
-        _poll.Start();
-    }
+        const int HealthPollMs = 500;
 
-    void Check()
-    {
-        var devs = PCAN_USB.GetUSBDevices();
-        bool present = devs.Count > 0;
+        readonly Timer      _healthTimer;
+        readonly IDispatcher _dispatcher;
 
-        if (present)
+        PCAN_USB? _dev;
+        ushort    _handle;
+
+        bool _isConnected;
+        public bool IsConnected
         {
-            _absentCount = 0;
+            get => _isConnected;
+            private set
+            {
+                if (_isConnected == value) return;
+                _isConnected = value;
+                PropertyChanged?.Invoke(this, new(nameof(IsConnected)));
+            }
+        }
+
+        public string? DeviceName { get; private set; }
+
+        public event Action<PCAN_USB.Packet>? FrameReceived;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public CanBusService(IDispatcher dispatcher)
+        {
+            _dispatcher   = dispatcher;
+            _healthTimer  = new Timer(HealthPollMs) { AutoReset = true };
+            _healthTimer.Elapsed += (_,__) => HealthCheck();
+            _healthTimer.Start();
+        }
+
+        void HealthCheck()
+        {
+            // 1) If not yet initialized, look for the stick and init once
             if (_dev == null)
-                TryInit(devs[0]);
-        }
-        else
-        {
-            _absentCount++;
-            if (_absentCount >= DebounceForUnplug && _dev != null)
-                Teardown();
-        }
-    }
-
-    void TryInit(string name)
-    {
-        try
-        {
-            DeviceName = name;
-            _h = PCAN_USB.DecodePEAKHandle(name);
-            _dev = new PCAN_USB();
-            var st = _dev.InitializeCAN(_h, "250 kbit/s", true);
-
-            if (st == TPCANStatus.PCAN_ERROR_OK)
             {
-                _dev.MessageReceived += OnPlatformMessageReceived;
-                IsConnected = true;
+                var devices = PCAN_USB.GetUSBDevices();
+                if (devices.Count > 0)
+                {
+                    DeviceName = devices[0];
+                    _handle    = PCAN_USB.DecodePEAKHandle(DeviceName);
+                    _dev       = new PCAN_USB();
+                    var status = _dev.InitializeCAN(_handle, "250 kbit/s", true);
+                    if (status == TPCANStatus.PCAN_ERROR_OK)
+                    {
+                        _dev.MessageReceived += pkt =>
+                            _dispatcher.Dispatch(() => FrameReceived?.Invoke(pkt));
+                        IsConnected = true;
+                    }
+                    else
+                    {
+                        // initialization failed: clean up
+                        _dev.Uninitialize();
+                        _dev = null;
+                    }
+                }
+                return;
             }
-            else
-            {
+
+            // 2) If already initialized, just check channel condition for live status
+            var ret = PCANBasic.GetValue(
+                _handle,
+                TPCANParameter.PCAN_CHANNEL_CONDITION,
+                out uint condition,
+                sizeof(uint)
+            );
+            bool alive = ret == TPCANStatus.PCAN_ERROR_OK
+                      && (condition & PCANBasic.PCAN_CHANNEL_AVAILABLE)
+                         == PCANBasic.PCAN_CHANNEL_AVAILABLE;
+            IsConnected = alive;
+        }
+
+        public void SendFrame(uint id, byte[] data, bool extended)
+        {
+            if (_dev != null && IsConnected)
+                _dev.WriteFrame(id, data.Length, data, extended);
+        }
+
+        public void Dispose()
+        {
+            _healthTimer.Stop();
+            _healthTimer.Dispose();
+            if (_dev != null)
                 _dev.Uninitialize();
-                _dev = null;
-            }
-        }
-        catch
-        {
-            _dev?.Uninitialize();
-            _dev = null;
         }
     }
-
-    void OnPlatformMessageReceived(PCAN_USB.Packet pkt)
-    {
-        // won’t fire after Teardown(), because we remove this handler there:
-        _dispatcher.Dispatch(() => FrameReceived?.Invoke(pkt));
-    }
-
-    void Teardown()
-    {
-        if (_dev != null)
-        {
-            _dev.MessageReceived -= OnPlatformMessageReceived;
-            _dev.Uninitialize();
-        }
-        _dev = null;
-        DeviceName = null;
-        IsConnected = false;
-    }
-
-    public void SendFrame(uint id, byte[] data, bool extended)
-    {
-        if (_dev != null && IsConnected)
-            _dev.WriteFrame(id, data.Length, data, extended);
-    }
-
-    public void Dispose()
-    {
-        _poll.Stop();
-        _poll.Dispose();
-        if (_dev != null)
-            _dev.Uninitialize();
-    }
+    #endif
 }
