b56bd2010fb1da3903698ed529092bbe2ad6b37a
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-25 10:35:00 -0400

    Update PCanService.cs

--- PCanService.cs
+++ PCanService.cs
@@ -1,37 +1,55 @@
+using PCANAppM;
 using Peak.Can.Basic;
 using System;
 
-namespace PCANAppM.Platforms.Windows
+#if WINDOWS
+using PCANAppM.Platforms.Windows;
+#endif
+
+namespace PCANAppM
 {
+#if WINDOWS
     public static class PCanService
     {
-        static PCanService()
-        {
-            // 1) Find the USB device
-            var devices = PCAN_USB.GetUSBDevices();
-            if (devices.Count == 0)
-                throw new InvalidOperationException("No PCAN USB devices found.");
-
-            // 2) Initialize it exactly once
-            var handle = PCAN_USB.DecodePEAKHandle(devices[0]);
-            Instance = new PCAN_USB();
-            var status  = Instance.InitializeCAN(handle, "250 kbit/s", true);
-            if (status != TPCANStatus.PCAN_ERROR_OK)
-                throw new InvalidOperationException(Instance.PeakCANStatusErrorString(status));
-
-            // 3) Expose the raw events
-            Instance.MessageReceived += pkt => MessageReceived?.Invoke(pkt);
-            Instance.Feedback        += msg => Feedback?.Invoke(msg);
-
-            IsStarted = true;
-        }
-
-        public static PCAN_USB Instance { get; }
-        public static bool     IsStarted { get; }
+        public static PCAN_USB? Instance { get; private set; }
+        public static bool IsStarted { get; private set; }
+        public static string? DeviceName { get; private set; }
 
         /// <summary>Fires on every incoming CAN frame.</summary>
         public static event Action<PCAN_USB.Packet>? MessageReceived;
         /// <summary>Fires on library feedback (errors, logs, etc.).</summary>
-        public static event Action<string>?         Feedback;
+        public static event Action<string>? Feedback;
+
+        public static bool TryInitialize()
+        {
+            try
+            {
+                var devices = PCAN_USB.GetUSBDevices();
+                if (devices.Count == 0)
+                    return false;
+
+                DeviceName = devices[0];
+                var handle = PCAN_USB.DecodePEAKHandle(devices[0]);
+                Instance = new PCAN_USB();
+                var status = Instance.InitializeCAN(handle, "250 kbit/s", true);
+                if (status != TPCANStatus.PCAN_ERROR_OK)
+                    return false;
+
+                Instance.MessageReceived += pkt => MessageReceived?.Invoke(pkt);
+                Instance.Feedback += msg => Feedback?.Invoke(msg);
+
+                IsStarted = true;
+                return true;
+            }
+            catch
+            {
+                // Optionally log or handle error
+                IsStarted = false;
+                return false;
+            }
+        }
     }
+#endif
 }
+
+
