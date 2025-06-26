6ed299f68722bb8194c7e7ee2021a48e2f1f8df7
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-26 18:03:09 -0400

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -39,3 +39,52 @@ namespace PCANAppM
     }
 }
 #endif
+
+#if WINDOWS
+using Microsoft.Extensions.Logging;
+using LocalizationResourceManager.Maui;
+using PCANAppM.Resources.Languages;
+using System.Globalization;
+using Syncfusion.Maui.Core.Hosting; 
+using PCANAppM.Services;
+
+namespace PCANAppM
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
+                .UseLocalizationResourceManager(settings =>
+                {
+                    settings.AddResource(PCANAppM.Resources.Languages.AppResources.ResourceManager);
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Barlow-Regular.ttf", "BarlowRegular");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            builder.Services.AddSingleton<ICanBusService, CanBusService>();
+            builder.Services.AddSingleton<ILocalizationResourceManager, LocalizationResourceManager.Maui.LocalizationResourceManager>();
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddTransient<Menu>();
+            builder.Services.AddTransient<BAS>();
+            builder.Services.AddTransient<KZV>();
+            builder.Services.AddTransient<FTLS>();
+
+
+
+            return builder.Build();
+        }
+    }
+}
+#endif
