c04476d2aa1e75f648ad94fcff74363918de2f2d
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-26 14:50:58 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -189,3 +189,73 @@ namespace PCANAppM
     }
 #endif
 }
+
+using Microsoft.Maui.Controls;
+using LocalizationResourceManager.Maui;
+using PCANAppM.Services;
+using System;
+
+namespace PCANAppM
+{
+#if WINDOWS
+    public partial class MainPage : ContentPage
+    {
+        readonly ILocalizationResourceManager _loc;
+        readonly CanBusService               _bus;
+        bool                                 _isDeviceConnected;
+
+        public MainPage(
+            ILocalizationResourceManager loc,
+            CanBusService               bus
+        )
+        {
+            InitializeComponent();
+            _loc = loc;
+            _bus = bus;
+
+            // live updates:
+            _bus.StatusChanged += OnStatusChanged;
+            UpdateUI();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _bus.StatusChanged -= OnStatusChanged;
+        }
+
+        void OnStatusChanged() =>
+            Microsoft.Maui.ApplicationModel.MainThread
+              .BeginInvokeOnMainThread(UpdateUI);
+
+        void UpdateUI()
+        {
+            bool c = _bus.IsConnected;
+            StatusLabel.Text   = c
+                ? $"{_bus.DeviceName}  {_loc["Status2"]}"
+                : _loc["Status1"];
+            StatusImage1.Source = c ? "green_check.png" : "red_ex.png";
+            _isDeviceConnected  = c;
+        }
+
+        async void OnStatusImageClicked(object sender, EventArgs e)
+        {
+            if (_isDeviceConnected)
+                await Navigation.PushAsync(new Menu(_loc, _bus));
+            else
+            {
+                ConnectionDialog.IsVisible = true;
+                MainContent.IsVisible      = false;
+            }
+        }
+
+        void OnConnectionDialogOkClicked(object sender, EventArgs e)
+        {
+            ConnectionDialog.IsVisible = false;
+            MainContent.IsVisible      = true;
+        }
+
+        // … rest of your side‐menu handlers …
+    }
+#endif
+}
