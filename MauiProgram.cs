7800b886a435a75a60a239b2a2756b3b299fb230
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-26 14:49:23 -0400

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -28,3 +28,26 @@ namespace PCANAppMaui
         }
     }
 }
+
+using PCANAppM.Services;
+// …
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { /* … */ });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        // ← add this line:
+        builder.Services.AddSingleton<CanBusService>();
+
+        return builder.Build();
+    }
+}
+
