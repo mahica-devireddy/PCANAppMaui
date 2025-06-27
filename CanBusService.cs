8f37e7862cffcd435fac8da0a066b4d844ab5224
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-27 10:59:31 -0400

    Update CanBusService.cs

--- CanBusService.cs
+++ CanBusService.cs
@@ -1,5 +1,7 @@
+#if WINDOWS
+
 using System;
-using System.Timers;
+using Timer = System.Timers.Timer;
 using Peak.Can.Basic;                       // make sure you’ve added the Peak.Can.Basic NuGet/package
 using TPCANHandle = System.UInt16;
 using TPCANStatus = Peak.Can.Basic.TPCANStatus;
@@ -8,28 +10,30 @@ namespace PCANAppM.Services
 {
     public class CanBusService : ICanBusService, IDisposable
     {
-        private readonly Timer      _timer;
+        private readonly Timer _timer;
         private readonly TPCANHandle _handle = PCANBasic.PCAN_USBBUS1;
-        private bool                 _isConnected;
+        private bool _isConnected;
+        private string _deviceName = "";
 
         public event EventHandler<bool> ConnectionStatusChanged;
         public bool IsConnected => _isConnected;
+        public string DeviceName => _deviceName;
 
         public CanBusService()
         {
-            // initialize the channel at 500 kbit/s (or whatever baud you need)
+            // initialize the channel at 250 kbit/s (or whatever baud you need)
             var initResult = PCANBasic.Initialize(
                 _handle,
-                TPCANBaudrate.PCAN_BAUD_500K
+                TPCANBaudrate.PCAN_BAUD_250K
             );
             // you can check initResult != PCAN_ERROR_OK to log/fail if you want
 
             _timer = new Timer(1000);
-            _timer.Elapsed += (_,__) => CheckStatus();
+            _timer.Elapsed += (_, __) => CheckStatus();
         }
 
         public void StartMonitoring() => _timer.Start();
-        public void StopMonitoring()  => _timer.Stop();
+        public void StopMonitoring() => _timer.Stop();
 
         private void CheckStatus()
         {
@@ -52,3 +56,4 @@ namespace PCANAppM.Services
         }
     }
 }
+#endif
