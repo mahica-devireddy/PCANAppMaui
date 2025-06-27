3a76e6e155e064280e6a02dfd42d38ebc3c46133
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-27 11:00:34 -0400

    Update CanBusService.cs

--- CanBusService.cs
+++ CanBusService.cs
@@ -1,23 +1,25 @@
 #if WINDOWS
 
 using System;
+using System.Text;
 using Timer = System.Timers.Timer;
 using Peak.Can.Basic;                       // make sure you’ve added the Peak.Can.Basic NuGet/package
-using TPCANHandle = System.UInt16;
-using TPCANStatus = Peak.Can.Basic.TPCANStatus;
+using TPCANHandle   = System.UInt16;
+using TPCANStatus   = Peak.Can.Basic.TPCANStatus;
+using TPCANParameter = Peak.Can.Basic.TPCANParameter;
 
 namespace PCANAppM.Services
 {
     public class CanBusService : ICanBusService, IDisposable
     {
-        private readonly Timer _timer;
-        private readonly TPCANHandle _handle = PCANBasic.PCAN_USBBUS1;
-        private bool _isConnected;
-        private string _deviceName = "";
+        private readonly Timer      _timer;
+        private readonly TPCANHandle _handle     = PCANBasic.PCAN_USBBUS1;
+        private bool                 _isConnected;
+        private string               _deviceName = "";
 
         public event EventHandler<bool> ConnectionStatusChanged;
-        public bool IsConnected => _isConnected;
-        public string DeviceName => _deviceName;
+        public bool   IsConnected  => _isConnected;
+        public string DeviceName   => _deviceName;
 
         public CanBusService()
         {
@@ -33,17 +35,38 @@ namespace PCANAppM.Services
         }
 
         public void StartMonitoring() => _timer.Start();
-        public void StopMonitoring() => _timer.Stop();
+        public void StopMonitoring()  => _timer.Stop();
 
         private void CheckStatus()
         {
             // static call—no instance
-            var status = PCANBasic.GetStatus(_handle);
+            var status       = PCANBasic.GetStatus(_handle);
             bool nowConnected = (status == TPCANStatus.PCAN_ERROR_OK);
 
             if (nowConnected != _isConnected)
             {
                 _isConnected = nowConnected;
+
+                if (_isConnected)
+                {
+                    // --- NEW: read the hardware name on connect ---
+                    var sb = new StringBuilder(PCANBasic.MAX_LENGTH_HARDWARE_NAME);
+                    var res = PCANBasic.GetValue(
+                        _handle,
+                        TPCANParameter.PCAN_HARDWARE_NAME,
+                        sb,
+                        PCANBasic.MAX_LENGTH_HARDWARE_NAME
+                    );
+                    _deviceName = (res == TPCANStatus.PCAN_ERROR_OK) 
+                                ? sb.ToString() 
+                                : "Unknown PCAN USB";
+                }
+                else
+                {
+                    _deviceName = "";
+                }
+                // --------------------------------------------------
+
                 ConnectionStatusChanged?.Invoke(this, _isConnected);
             }
         }
