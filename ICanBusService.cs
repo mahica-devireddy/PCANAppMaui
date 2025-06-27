5f83396f4fc017936904f28ed4d7cb180c060d21
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-27 09:09:08 -0400

    Update ICanBusService.cs

--- ICanBusService.cs
+++ ICanBusService.cs
@@ -3,26 +3,18 @@
 using System;
 using Peak.Can.Basic;
 using System;
+using System.ComponentModel;
 using PCANAppM.Platforms.Windows;
 
 namespace PCANAppM.Services
 {
 
-    public interface ICanBusService
+    public interface ICanBusService : INotifyPropertyChanged, IDisposable
     {
-        /// <summary>True when the PCAN USB is plugged in AND initialized.</summary>
         bool IsConnected { get; }
-
-        /// <summary>The device string from GetUSBDevices()[0], or null if none.</summary>
         string? DeviceName { get; }
-
-        /// <summary>Fires whenever IsConnected changes (debounced).</summary>
-        event Action? StatusChanged;
-
-        /// <summary>Fires on every incoming CAN frame (on the UI thread).</summary>
+        //event Action? StatusChanged;
         event Action<PCAN_USB.Packet>? FrameReceived;
-
-        /// <summary>Send a CAN frame on the shared bus.</summary>
         void SendFrame(uint id, byte[] data, bool extended);
     }
 }
