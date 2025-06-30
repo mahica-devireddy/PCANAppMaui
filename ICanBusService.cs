502621bea52c43aa61578155ae31557599193dd8
mahica-devireddy <mahica.devireddy@gmail.com>
2025-06-30 07:46:45 -0400

    Update ICanBusService.cs

--- ICanBusService.cs
+++ ICanBusService.cs
@@ -9,13 +9,27 @@ using PCANAppM.Platforms.Windows;
 namespace PCANAppM.Services
 {
 
-    public interface ICanBusService : INotifyPropertyChanged, IDisposable
+    public interface ICanBusService : IDisposable
     {
+        //bool IsConnected { get; }
+        //string? DeviceName { get; }
+        //event Action? StatusChanged;
+        //event Action<PCAN_USB.Packet>? FrameReceived;
+        //void SendFrame(uint id, byte[] do data, bool extended);
+
+        event EventHandler<bool> ConnectionStatusChanged;
+
         bool IsConnected { get; }
-        string? DeviceName { get; }
-        event Action? StatusChanged;
-        event Action<PCAN_USB.Packet>? FrameReceived;
-        void SendFrame(uint id, byte[] data, bool extended);
+
+        string DeviceName { get; }
+
+        void StartMonitoring();
+
+        void StopMonitoring();
+
+        //TPCANStatus SendFrame(uint id, int dataLength, byte[] data);
+        TPCANStatus ReadMessages(Action<TPCANMsg, TPCANTimestamp> onMessageReceived);
+
     }
 }
 #endif
