05c87e86f084c410cf4996bb9a0e319cbac1c198
mahica-devireddy <mahica.devireddy@gmail.com>
2025-07-02 09:40:25 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,104 @@
+private void OnLanguageButtonClicked(object sender, EventArgs e)
+        {
+            LanguageState.CurrentLanguage = LanguageState.CurrentLanguage == "en" ? "es" : "en";
+            _localizationResourceManager.CurrentCulture = new CultureInfo(LanguageState.CurrentLanguage);
+        }
 
+        private async void OnNextButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Menu(_localizationResourceManager));
+        }
+
+        private void StatusImage1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnOshkoshLogoClicked(object sender, EventArgs e)
+        {
+            SideMenu.IsVisible = true;
+            SideMenuDim.IsVisible = true;
+
+            if (SideMenu.Width == 0)
+            {
+                // Wait for the menu to be measured, then animate
+                SideMenu.SizeChanged += SideMenu_SizeChangedAnimateIn;
+            }
+            else
+            {
+                AnimateSideMenuIn();
+            }
+        }
+
+        private async void SideMenu_SizeChangedAnimateIn(object? sender, EventArgs e)
+        {
+            if (SideMenu.Width > 0)
+            {
+                SideMenu.SizeChanged -= SideMenu_SizeChangedAnimateIn;
+                await AnimateSideMenuIn();
+            }
+        }
+
+        private async Task AnimateSideMenuIn()
+        {
+            SideMenu.TranslationX = -SideMenu.Width;
+            await SideMenu.TranslateTo(0, 0, 250, Easing.SinOut);
+        }
+
+        private async void SideMenuOnFirstSizeChanged(object? sender, EventArgs e)
+        {
+            SideMenu.SizeChanged -= SideMenuOnFirstSizeChanged;
+            _sideMenuFirstOpen = false;
+            SideMenu.TranslationX = -SideMenu.Width;
+            await SideMenu.TranslateTo(0, 0, 250, Easing.SinOut);
+        }
+
+        private async void OnCloseSideMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 250, Easing.SinIn); // Slide out
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+        }
+
+        private async void OnMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new Menu(_localizationResourceManager));
+        }
+
+        private async void OnAngleSensorMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new BAS(_localizationResourceManager));
+        }
+
+        private async void OnKzValveMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new KZV(_localizationResourceManager));
+        }
+
+        private async void OnFluidTankLevelMenuClicked(object sender, EventArgs e)
+        {
+            await SideMenu.TranslateTo(-SideMenu.Width, 0, 200, Easing.SinIn);
+            SideMenu.IsVisible = false;
+            SideMenuDim.IsVisible = false;
+            await Navigation.PushAsync(new FTLS(_localizationResourceManager));
+        }
+    }
+
+    public static class LanguageState
+    {
+        public static string CurrentLanguage { get; set; } = "en";
+        public static bool IsSpanish => CurrentLanguage == "es";
+    }
+}
 #if WINDOWS
 using System;
 using System.Collections.Generic;
